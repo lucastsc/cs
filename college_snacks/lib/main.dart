87b487427529f29978499e102ca0aa198105aa95
szerwinski <hugoszervinsk@gmail.com>
2019-04-27 20:27:59 -0300

    start for the project

--- college_snacks/lib/main.dart
+++ college_snacks/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:college_snacks/screens/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main(){
 
   runApp(MaterialApp(
-    /*home: HomePage(),*/
+    home: HomeScreen(),
     debugShowCheckedModeBanner: false,
   ));
 }

140148fa51692b8dd74e909e3664249923fba48a
lucas <lucastsc@gmail.com>
2019-05-11 10:01:10 -0300

    added product quantity control

--- college_snacks/lib/Tabs/final_product_tab.dart
+++ college_snacks/lib/Tabs/final_product_tab.dart
@@ -2,8 +2,8 @@ import 'package:college_snacks/datas/product_data.dart';
 import 'package:flutter/material.dart';
 
 class FinalProductTab extends StatefulWidget {
-
   ProductData productData;
+
   FinalProductTab(this.productData);
 
   @override
@@ -12,16 +12,16 @@ class FinalProductTab extends StatefulWidget {
 
 class _FinalProductTabState extends State<FinalProductTab> {
 
+  int quantity = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
+      appBar: AppBar(),
       body: Container(
         child: Column(
           children: <Widget>[
-            Row(
+            Row(//row containing the image of the product
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
@@ -29,27 +29,67 @@ class _FinalProductTabState extends State<FinalProductTab> {
                   height: 200.0,
                   decoration: BoxDecoration(
                       image: DecorationImage(
-                          image: NetworkImage(widget.productData.url)
-                      )
-                  ),
+                          image: NetworkImage(widget.productData.url))),
                 )
               ],
             ),
-            Row(
+            Row(//row containing the description of the product
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(widget.productData.description)
-              ],
+              children: <Widget>[Text(widget.productData.description)],
             ),
             SizedBox(
               height: 30.0,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Row(//containing the container with borders with the add and remove buttons
               children: <Widget>[
+                Container(//container with borders, with add and remove buttons
+                  height: 40.0,
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))
+                  ),
+                  child: Row(//row with [-button][value][+button]
+                    children: <Widget>[
+                      Container(//container containing the remove button
+                        height: 40.0,
+                        width: 30.0,
+                        child: FlatButton(
+                          padding: EdgeInsets.all(0),
+                          child: Icon(Icons.remove),
+                          onPressed: () {
+                            setState(() {
+                              quantity <= 0 ? quantity = 0 : quantity-=1;
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(//to give some space between buttons
+                        width: 10.0,
+                      ),
+                      Text("$quantity"),
+                      SizedBox(//to give some space between buttons
+                        width: 10.0,
+                      ),
+                      Container(//container containing the remove button
+                        height: 40.0,
+                        width: 30.0,
+                        child: FlatButton(
+                          padding: EdgeInsets.all(0),
+                          child: Icon(Icons.add),
+                          onPressed: () {
+                            setState(() {
+                              quantity+=1;
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
                 RaisedButton(
                   child: Text("Adicionar ao carrinho"),
-                  onPressed: (){},
+                  onPressed: () {},
                 )
               ],
             )

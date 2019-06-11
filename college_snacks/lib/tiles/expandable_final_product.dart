c1402ace5aa03b0c72070215719553ce96e85ed2
szerwinski <hugoszervinsk@gmail.com>
2019-06-11 20:58:00 -0300

    Made minor changes to ExpandableFinalProduct, removing unnecessary rows inside main column

--- college_snacks/lib/tiles/expandable_final_product.dart
+++ college_snacks/lib/tiles/expandable_final_product.dart
@@ -45,111 +45,95 @@ class _ExpandableFinalProductState extends State<ExpandableFinalProduct> {
 
     return Container(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Row(//row containing the image of the product
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 200.0,
-                height: 200.0,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(product.url))),
-              )
-            ],
+          Container(
+            width: 200.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(product.url))),
           ),
-          Row(//row containing the description of the product
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Expanded(child: Container(alignment:Alignment.center,margin:EdgeInsets.all(20.0),child: Text(product.description,style: TextStyle(fontSize: 18.0),),),)],
+          Container(
+            alignment:Alignment.center,
+            margin:EdgeInsets.all(20.0),
+            child: Text(product.description,style: TextStyle(fontSize: 18.0),),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text("R\$:$productPriceShown" ,style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),)],
+          Text(
+            "R\$:$productPriceShown" ,
+            style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),
           ),
           SizedBox(//vertical space
             height: 10.0,
           ),
-          Row(//containing the container with borders with the add and remove buttons
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(//container with borders, with add and remove buttons
-                height: 40.0,
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))
-                ),
-                child: Row(//row with [-button][value][+button]
-                  children: <Widget>[
-                    Container(//container containing the remove button
-                      height: 40.0,
-                      width: 30.0,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(0),
-                        child: Icon(Icons.remove),
-                        onPressed: () {
-                          setState(() {
-                            quantity <= 1 ? quantity = 1 : quantity-=1;
-                          });
-                        },
-                      ),
-                    ),
-                    SizedBox(//to give some space between buttons
-                      width: 10.0,
-                    ),
-                    Text("$quantity"),
-                    SizedBox(//to give some space between buttons
-                      width: 10.0,
-                    ),
-                    Container(//container containing the remove button
-                      height: 40.0,
-                      width: 30.0,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(0),
-                        child: Icon(Icons.add),
-                        onPressed: () {
-                          setState(() {
-                            quantity+=1;
-                          });
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text("Adicionar ao carrinho"),
+          Container(//container with borders, with add and remove buttons
+            height: 40.0,
+            margin: EdgeInsets.symmetric(horizontal: 110.0, vertical: 15.0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(10.0))
+            ),
+            child: Row(//row with [-button][value][+button]
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(//container containing the remove button
+                  height: 40.0,
+                  width: 30.0,
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0),
+                    child: Icon(Icons.remove),
                     onPressed: () {
-                      if(UserModel.of(context).isLoggedIn()){
-                        //add to the cart
-                        CartProduct cartProduct = CartProduct();
-                        cartProduct.quantity = quantity;
-                        cartProduct.pid = product.id;
-                        cartProduct.category = category.id; // product category id
-                        cartProduct.productData = product;
-
-
-                        Scaffold.of(context).showSnackBar(//modified this line to use the SnackBar without the Scaffold.We could delete the GlobalKey in the beggining of the file
-                            SnackBar(content: Text("Item adicionado com sucesso!"), duration: Duration(seconds: 2),backgroundColor: Theme.of(context).primaryColor,)
-                        );
-                        CartModel.of(context).addCartItem(cartProduct);
-                      }else{
-                        Navigator.of(context).push(MaterialPageRoute(builder:(context)=>LoginScreen()));
-                      }
+                      setState(() {
+                        quantity <= 1 ? quantity = 1 : quantity-=1;
+                      });
                     },
                   ),
-                  Text("Total: R\$: $totalPriceShown",style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),)
-                ],
-              )
-            ],
-          )
+                ),
+                SizedBox(//to give some space between buttons
+                  width: 10.0,
+                ),
+                Text("$quantity"),
+                SizedBox(//to give some space between buttons
+                  width: 10.0,
+                ),
+                Container(//container containing the remove button
+                  height: 40.0,
+                  width: 30.0,
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0),
+                    child: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        quantity+=1;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+          RaisedButton(
+            child: Text("Adicionar ao carrinho"),
+            onPressed: () {
+              if(UserModel.of(context).isLoggedIn()){
+                //add to the cart
+                CartProduct cartProduct = CartProduct();
+                cartProduct.quantity = quantity;
+                cartProduct.pid = product.id;
+                cartProduct.category = category.id; // product category id
+                cartProduct.productData = product;
+
+
+                Scaffold.of(context).showSnackBar(//modified this line to use the SnackBar without the Scaffold.We could delete the GlobalKey in the beggining of the file
+                    SnackBar(content: Text("Item adicionado com sucesso!"), duration: Duration(seconds: 2),backgroundColor: Theme.of(context).primaryColor,)
+                );
+                CartModel.of(context).addCartItem(cartProduct);
+              }else{
+                Navigator.of(context).push(MaterialPageRoute(builder:(context)=>LoginScreen()));
+              }
+            },
+          ),
+          Text("Total: R\$: $totalPriceShown",style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),)
         ],
       ),
     );
